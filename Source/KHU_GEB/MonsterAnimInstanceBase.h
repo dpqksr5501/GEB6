919b44504a75053f85cbae4e189d117b04542904
그림자_군주_조연우 <ckhlight2@gmail.com>
2025-10-13 01:06:51 +0900

    Modify Monster
    C++ 환경에 맞게 설정했습니다.

---
+++ Source/KHU_GEB/MonsterAnimInstanceBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MonsterAnimInstanceBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KHU_GEB_API UMonsterAnimInstanceBase : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
