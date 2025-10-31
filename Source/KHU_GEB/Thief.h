71f747c6199ff0c62b3e7ee27d687a8bff92a95e
그림자_군주_조연우 <ckhlight2@gmail.com>
2025-10-31 20:44:23 +0900

    Add Thief
    도적 추가했습니다.

---
+++ Source/KHU_GEB/Thief.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MonsterBase.h"
+#include "Thief.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KHU_GEB_API AThief : public AMonsterBase
+{
+	GENERATED_BODY()
+	
+public:
+	AThief();
+};
