f16b41f479b5e2b253b537303e694dd687c02366
ChangBeom Seo <fken0116@khu.ac.kr>
2025-11-03 13:06:25 +0900

    merge

---
+++ Source/KHU_GEB/Wizard.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Wizard.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+AWizard::AWizard() {
+}
