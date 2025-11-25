b974ac1546a8a0ab7c2d984973856026640dcd06
Jinsoo3812 <vlvkrhksgml3812@naver.com>
2025-11-25 22:18:47 +0900

    Dragon 적용. 이것도 나이아가라 없어서 되는건지..

---
+++ Source/KHU_GEB/Enemy_AI/Enemy_Dragon.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy_AI/Enemy_Base.h"
+#include "Enemy_Dragon.generated.h"
+
+class AFireballProjectile; 
+
+/**
+ * 
+ */
+UCLASS()
+class KHU_GEB_API AEnemy_Dragon : public AEnemy_Base
+{
+	GENERATED_BODY()
+
+public:
+	AEnemy_Dragon();
+
+	virtual void ActivateSkill() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dragon|Skill")
+	TSubclassOf<AFireballProjectile> FireballClass;
+
+protected:
+	virtual void BeginPlay() override;
+};
